dc8c56786ddaab4dd235ebbdad8efaad09be4665
jonathanwong110 <jonathan.wong110@gmail.com>
2020-12-13 17:50:14 -0500

    created a sidebar, a dashboard component, a weather component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Sidebar from './components/navigation/sidebar/Sidebar';
+import Dashboard from './components/Dashboard'
+import Weather from './components/Weather'
 
 class App extends Component {
+
   render() {
     return (
-      <div>Testing</div>
+      <BrowserRouter>
+        <div className="App">
+          <Sidebar />
+          <div style={{ height: "90px", width: "0px" }}></div>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/weather" component={Weather} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
 
-export default App;
+export default App
