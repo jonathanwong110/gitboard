bd3e10e1f04756f1b87425c48d36d4199374ed12
jonathanwong110 <jonathan.wong110@gmail.com>
2020-12-15 14:10:24 -0500

    put in a city in the weather api until query function is installed, displayed information fetched from the weather API

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -4,7 +4,7 @@ import { geolocated } from "react-geolocated";
 import { getWeatherForecast } from '../../redux/Weather/actions'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from 'react-bootstrap'
+import { Button, Image } from 'react-bootstrap'
 
 class Weather extends Component {
 
@@ -15,6 +15,10 @@ class Weather extends Component {
       searchQuery: '',
       loading: false,
     }
+  }
+
+  componentDidMount() {
+    this.props.getWeatherForecast()
   }
 
   handleChange = (e) => {
@@ -38,18 +42,44 @@ class Weather extends Component {
     })
   }
 
+  giveAdvice = (degrees) => {
+    if (degrees < 40) {
+      return <div> Bundle up. It's gonna be freezing! </div>
+    } else if (degrees >= 40 && degrees < 60) {
+      return <div> Wear something warm. It's gonna be chilly! </div>
+    } else if (degrees >= 60 && degrees <= 80) {
+      return <div> The weather's perfect! </div>
+    } else if (degrees > 81) {
+      return <div> It's gonna be be hot today. Stay cool! </div>
+    }
+  }
+
   render() {
+
+    let { weather } = this.props
+
+    if (weather && weather?.main === undefined) {
+      return <div>There is no weather here</div>
+    }
 
     return (
       <>
+        {console.log(weather)}
         <div className="page-heading">Weather Page</div>
         <div>Find the weather for any city</div>
+        <br></br>
         <div className="weather-container">
           <br></br>
           <input type="text" name="search" className="searchForm" placeholder="Search a City" onKeyPress={this.onKeyPress} onChange={e => this.handleChange(e)} />
           <Button variant="primary" className="searchButton" type="submit" onClick={e => this.handleSubmit(e)}>
             <FontAwesomeIcon icon={faSearch} />
           </Button>
+          <br></br>
+          <Image src={"http://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png"} style={{ height: "75px", width: "75px" }} />
+          <div>{weather.main.temp} &deg;F </div>
+          <div>Low {weather.main.temp_min} &deg;F  / Low {weather.main.temp_max} &deg;F </div>
+          <div>{this.giveAdvice(weather.main.temp)}</div>
+          <div>{weather.name}, {weather.sys.country}</div>
         </div>
       </>
     )
--- src/redux/Weather/actions.js
+++ src/redux/Weather/actions.js
@@ -4,9 +4,10 @@ import { weatherAPI } from '../../API/keys'
 
 export const getWeatherForecast = (query) => {
   return (dispatch) => {
-    axios.get(`${weatherAPI.base}weather?q=${query}&units=imperial&APPID=${weatherAPI.key}`).then(res => {
+    axios.get(`${weatherAPI.base}q=Brooklyn&units=imperial&APPID=${weatherAPI.key}`).then(res => {
       dispatch({ type: WeatherActionTypes.GET_WEATHER_FORECAST, weather: res.data })
-    }
-    )
+    }).catch(function (error) {
+      alert(error)
+    });
   }
 }
