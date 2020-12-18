4079e602167674f43eeb58cecce18a284b7d1054
jonathanwong110 <jonathan.wong110@gmail.com>
2020-12-18 13:50:34 -0500

    still working on how stock info will appear

--- src/redux/Stocks/actions.js
+++ src/redux/Stocks/actions.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import StocksActionTypes from "./types";
 import { stocksAPI } from '../../API/keys'
 
-export const getStocks = () => {
+export const getStocks = (query) => {
   return (dispatch) => {
-    axios.get("").then(res => {
+    axios.get(`${stocksAPI.base}${query}&apikey=${stocksAPI.key}`).then(res => {
       dispatch({ type: StocksActionTypes.GET_STOCKS, stocks: res.data })
     }
     )
